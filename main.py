251e2ee48e30a3fc618304c0e4209cb584872ba8
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-11-17 14:25:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from pyrogram import Client
 import config
 
-app = Client(
-    "stock_bot",
-    bot_token=config.BOT_TOKEN,
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    workers=50,
-)
+class StockBot(Client):
+    def __init__(self):
+        super().__init__(
+            name="stock_bot",
+            bot_token=config.BOT_TOKEN,
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            workers=50,
+        )
 
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        print(f"Bot {me.username} is running...")
+
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot is stopping...")
+
+# Run the bot
 if __name__ == "__main__":
-    print("Bot is running...")
+    app = StockBot()
     app.run()
