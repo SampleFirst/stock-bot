5d32949d17add79c870cceee726407176a52a191
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-11-17 13:37:48 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -1,7 +1,5 @@
-
 from pyrogram import Client, filters
 
+@Client.on_message(filters.command("help"))
 async def help(client, message):
     await message.reply("Send a stock name or ticker symbol to get its current price and recommendations.")
-
-handler = filters.command("help")(help)
