a458c05fa60db58c63d88a13c6de2f17de59d1c7
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-11-17 13:31:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-STOCK_API_KEY = os.getenv("STOCK_API_KEY")
-MONGO_URI = os.getenv("MONGO_URI")
+BOT_TOKEN = os.getenv("7417084160:AAEgqGZxQBkCJKETr1KUMfsYVAr2gfcRYAs")
+STOCK_API_KEY = os.getenv("cssq4d1r01qld5m1h8fgcssq4d1r01qld5m1h8g0")
+MONGO_URI = os.getenv("mongodb+srv://Rename:Rename@cluster0.m3eacgp.mongodb.net/?retryWrites=true&w=majority")
