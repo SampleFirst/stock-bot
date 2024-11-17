416a79f7e56abe47cb2ffa87246d656c07f1f15c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-11-17 13:37:26 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,5 @@
-
 from pyrogram import Client, filters
 
+@Client.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply("Welcome to Stock Bot! Send me a stock name to get its report.")
-
-handler = filters.command("start")(start)
