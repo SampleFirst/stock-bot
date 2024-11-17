fc0982eaf1e3d7e1c7407b9b9a64852ff438aef0
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-11-17 13:38:14 +0530

    Update stock_query.py

--- handlers/stock_query.py
+++ handlers/stock_query.py
@@ -1,8 +1,8 @@
-
 from pyrogram import Client, filters
 from utils.stock_api import get_stock_report
 from utils.db import save_query
 
+@Client.on_message(filters.text & filters.private & ~filters.command(["start", "help"]))
 async def stock_query(client, message):
     query = message.text.strip()
     if not query:
@@ -11,11 +11,7 @@ async def stock_query(client, message):
 
     report = get_stock_report(query)
     if report:
-        await message.reply(f"Stock Report for {query}:
-
-{report}")
+        await message.reply(f"Stock Report for {query}:\n\n{report}")
         save_query(message.chat.id, query)
     else:
         await message.reply("Sorry, I couldn't find details for that stock.")
-
-handler = filters.text & filters.private & ~filters.command(["start", "help"])(stock_query)
